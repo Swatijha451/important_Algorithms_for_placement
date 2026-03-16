101696d8c6db83ee7081f47960ab660ec829b15d
Swati Jha <swatijha451@gmail.com>
2026-03-16 23:20:39 +0530

    2dArray

---
+++ 2darray/2darray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows = 4;
+    int cols = 3;
+    int arr[rows][cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
